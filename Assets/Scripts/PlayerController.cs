206ba1fb670694d0b8c40be41372a1284f7f4a2b
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-10-11 15:17:28 +0100

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 lookingDirection = Camera.main.transform.forward;
+        transform.Translate(lookingDirection * verticalInput * moveSpeed * Time.deltaTime);
+    }
+}
