40d52c18ac03ab765caed40404243591132bbe9a
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-10-12 13:55:42 +0100

    remove y component from player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 lookingDirection = Camera.main.transform.forward;
+
+        float xDirection = Camera.main.transform.forward.x;
+        float zDirection = Camera.main.transform.forward.z;
+
+        Vector3 lookingDirection = new Vector3(xDirection, 0, zDirection);
+        
         transform.Translate(lookingDirection * verticalInput * moveSpeed * Time.deltaTime);
     }
 }
