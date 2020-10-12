0500aa0fbf3f0f1d847051a885550ddc6e0730a3
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-10-12 15:19:01 +0100

    add flashlight toggle script

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private bool isOn;
+    private Light light;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            isOn = !isOn;
+            light.enabled = isOn;
+        }
+    }
+}
