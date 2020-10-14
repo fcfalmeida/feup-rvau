7b9ed7d1c28acc7095a4e320f20858be03c0e99d
Miguel Castro <1160574@isep.ipp.pt>
2020-10-14 22:38:52 +0100

    Add heartbeat rate related to distance

--- Assets/Scripts/Heartbeat.cs
+++ Assets/Scripts/Heartbeat.cs
@@ -6,25 +6,69 @@ public class Heartbeat : MonoBehaviour
 {
   private AudioSource heartbeat;
 
+  private GameObject player;
+  private GameObject slenderman;
+  private static float MIN_HEARBEAT_PITCH = 0.6f;
+  private static float MAX_HEARTBEAT_PITCH = 1.3f;
+  private static float MIN_DISTANCE = 10f;
+
   // Start is called before the first frame update
   void Start()
   {
     heartbeat = GetComponent<AudioSource>();
+    player = GameObject.Find("Player");
+    slenderman = GameObject.Find("Test");
   }
 
   // Update is called once per frame
   void Update()
   {
-    if (Input.GetKeyDown("u"))
+    float z = player.transform.position.z - slenderman.transform.position.z;
+    float y = player.transform.position.y - slenderman.transform.position.y;
+    float x = player.transform.position.x - slenderman.transform.position.x;
+    float distance = Mathf.Pow((Mathf.Pow(z, 2) + Mathf.Pow(y, 2) + Mathf.Pow(x, 2)), 0.5f);
+    /*if (distance < 5.0f)
     {
-      heartbeat.pitch += 0.1f;
+      heartbeat.pitch = 1.2f;
+      return;
+    }
+    if (distance < 7.0f)
+    {
+      heartbeat.pitch = 1.1f;
+      return;
+    }
+    if (distance < 10.0f)
+    {
+      heartbeat.pitch = 1.0f;
+      return;
+    }
+    if (distance < 12.0f)
+    {
+      heartbeat.pitch = 0.9f;
+      return;
+    }
+    if (distance < 14.0f)
+    {
+      heartbeat.pitch = 0.9f;
+      return;
+    }
+    if (distance < 16.0f)
+    {
+      heartbeat.pitch = 0.8f;
+      return;
+    }
+    if (distance < 20.0f)
+    {
+      heartbeat.pitch = 0.7f;
+      return;
     }
     else
     {
-      if (Input.GetKeyDown("d"))
-      {
-        heartbeat.pitch -= 0.1f;
-      }
-    }
+      heartbeat.pitch = 0.6f;
+      return;
+    }*/
+    float heartbeatPitch = MIN_DISTANCE / distance;
+    heartbeatPitch = heartbeatPitch > MAX_HEARTBEAT_PITCH ? MAX_HEARTBEAT_PITCH : heartbeatPitch;
+    heartbeat.pitch = heartbeatPitch < MIN_HEARBEAT_PITCH ? MIN_HEARBEAT_PITCH : heartbeatPitch;
   }
 }
