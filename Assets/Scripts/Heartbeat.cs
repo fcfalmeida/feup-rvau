974c1d193970a908d3ba546d80368f4435d8c80e
Miguel Castro <1160574@isep.ipp.pt>
2020-10-14 15:36:19 +0100

    Add heartbeat to player

---
+++ Assets/Scripts/Heartbeat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heartbeat : MonoBehaviour
+{
+  private AudioSource heartbeat;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    heartbeat = GetComponent<AudioSource>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown("u"))
+    {
+      heartbeat.pitch += 0.1f;
+    }
+    else
+    {
+      if (Input.GetKeyDown("d"))
+      {
+        heartbeat.pitch -= 0.1f;
+      }
+    }
+  }
+}
